033f6d510811be1e5dc5c48148db8e86184bec2b
unknown <naultmichael1@gmail.com>
2019-06-15 16:45:48 -0400

    config: conection and orm in place

--- config/connection.js
+++ config/connection.js
@@ -4,5 +4,11 @@ var connection = mysql.createConnection({
     host: "localhost",
     use: "root",
     password: "root",
-    database: "burger_db"
+    database: "burgers_db"
 });
+connection.connect(function(err) {
+    if (err) throw err; 
+    
+    console.log("connection as id " + connection.threadId)
+});
+module.exports = connection;
